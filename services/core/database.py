708f14cff92482f7f5faaee0977fdf2b2f07bab1
Rivindu Ashinsa <ashinsa.rivindu@gmail.com>
2025-05-30 19:23:22 +0530

    incident handling

--- services/core/database.py
+++
@@ -1,5 +0,0 @@
-# core/database.py
-from supabase import create_client, Client
-from core.config import SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
--- services/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from api.endpoints import users
-
-app = FastAPI()
-
-app.include_router(users.router, prefix="/api/users", tags=["Users"])
